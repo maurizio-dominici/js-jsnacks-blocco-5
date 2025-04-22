7bd9d8f1b6c63b7795dea0a25c0f399a8a72eac8
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-22 15:06:17 +0200

    add ex 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,7 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const numberPlusOne = numbers.map((element) => element + 1);
+console.log(numberPlusOne);
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]

9729fdb03ba5ebc18d1c775b72164c791162ad34
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-22 14:46:50 +0200

    add ex 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,11 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+const namePerson = people.map((person) => person.name);
+console.log(namePerson);
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'

c1e46eef84f38889ba56de1d52e1400cec10e860
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-22 15:12:32 +0200

    add ex 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,29 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
+
+const marcoStudent = students
+  .filter((beginner) => beginner.name === "Marco Lanci")
+  .map((beginner) => beginner.class);
+
+console.log(marcoStudent);
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
